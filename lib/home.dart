c46aea10926fd1e84fb9c180f3f59e0e7e75e8fa
Aavash <aavashpaudel10@gmail.com>
2021-06-27 20:57:53 +0545

    search front end

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:we_chat/helper/sharedpref.dart';
 import 'package:we_chat/services/auth.dart';
 import 'package:we_chat/signIn.dart';
 
@@ -9,6 +10,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isSearching = false;
+  String myName, myProfilePic, myUserName, myEmail;
+  TextEditingController searchtextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +35,59 @@ class _HomeState extends State<Home> {
               ),
             ),
           )),
-      drawer: Drawer(),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                isSearching
+                    ? GestureDetector(
+                        onTap: () {
+                          isSearching = false;
+                          searchtextEditingController.text = '';
+                          setState(() {});
+                        },
+                        child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            child: Icon(Icons.arrow_back)),
+                      )
+                    : Container(),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10),
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        )),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none, hintText: 'username'),
+                          ),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              isSearching = true;
+                              setState(() {});
+                            },
+                            child: Icon(Icons.search))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+
+      // drawer: Drawer(),
     );
   }
 }
